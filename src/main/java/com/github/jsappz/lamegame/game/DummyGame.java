6d42fd5f177e6faabbcb75e4f45fc1e75a38c962
Sage Smith <sagersmith8@gmail.com>
2020-01-26 18:58:38 -0700

    Added spotlights

--- src/main/java/com/github/jsappz/lamegame/game/DummyGame.java
+++ src/main/java/com/github/jsappz/lamegame/game/DummyGame.java
@@ -13,18 +13,25 @@ import static org.lwjgl.glfw.GLFW.*;
 public class DummyGame implements GameLogic {
     private static final float CAMERA_POS_STEP = 0.05f;
     private static final float MOUSE_SENSITIVITY = 0.2f;
+    private static final Vector3f WHITE_LIGHT = new Vector3f(1, 1, 1);
 
     private final Vector3f cameraInc;
     private final Renderer renderer;
     private final Camera camera;
     private GameItem[] gameItems;
     private Vector3f ambientLight;
-    private PointLight pointLight;
+    private PointLight[] pointLightList;
+    private SpotLight[] spotLightList;
+    private DirectionalLight directionalLight;
+    private float lightAngle;
+    private float spotAngle = 0;
+    private float spotInc = 1;
 
     public DummyGame() {
         renderer = new Renderer();
         camera = new Camera();
         cameraInc = new Vector3f(0.0f, 0.0f, 0.0f);
+        lightAngle = -90;
     }
 
     @Override
@@ -43,13 +50,26 @@ public class DummyGame implements GameLogic {
         gameItems = new GameItem[]{gameItem};
 
         ambientLight = new Vector3f(0.3f, 0.3f, 0.3f);
-        Vector3f lightColour = new Vector3f(1, 1, 1);
+        // Point Light
         Vector3f lightPosition = new Vector3f(0, 0, 1);
         float lightIntensity = 1.0f;
-        pointLight = new PointLight(lightColour, lightPosition, lightIntensity);
+        PointLight pointLight = new PointLight(WHITE_LIGHT, lightPosition, lightIntensity);
         PointLight.Attenuation att = new PointLight.Attenuation(0.0f, 0.0f, 1.0f);
         pointLight.setAttenuation(att);
-        pointLight.setPosition(new Vector3f(0, 1, 0));
+        pointLightList = new PointLight[]{pointLight};
+
+        // Spot Light
+        lightPosition = new Vector3f(0, 0.0f, 10f);
+        pointLight = new PointLight(WHITE_LIGHT, lightPosition, lightIntensity);
+        att = new PointLight.Attenuation(0.0f, 0.0f, 0.02f);
+        pointLight.setAttenuation(att);
+        Vector3f coneDir = new Vector3f(0, 0, -1);
+        float cutoff = (float) Math.cos(Math.toRadians(140));
+        SpotLight spotLight = new SpotLight(pointLight, coneDir, cutoff);
+        spotLightList = new SpotLight[]{spotLight, new SpotLight(spotLight)};
+
+        lightPosition = new Vector3f(-1, 0, 0);
+        directionalLight = new DirectionalLight(WHITE_LIGHT, lightPosition, lightIntensity);
     }
 
     @Override
@@ -71,11 +91,11 @@ public class DummyGame implements GameLogic {
             cameraInc.y = 1;
         }
 
-        float lightPos = pointLight.getPosition().z;
+        float lightPos = spotLightList[0].getPointLight().getPosition().z;
         if (window.isKeyPressed(GLFW_KEY_N)) {
-            this.pointLight.getPosition().z = lightPos + 0.1f;
+            this.spotLightList[0].getPointLight().getPosition().z = lightPos + 0.1f;
         } else if (window.isKeyPressed(GLFW_KEY_M)) {
-            this.pointLight.getPosition().z = lightPos - 0.1f;
+            this.spotLightList[0].getPointLight().getPosition().z = lightPos - 0.1f;
         }
     }
 
@@ -89,11 +109,44 @@ public class DummyGame implements GameLogic {
             Vector2f rotVec = mouseInput.getDisplVec();
             camera.moveRotation(rotVec.x * MOUSE_SENSITIVITY, rotVec.y * MOUSE_SENSITIVITY, 0);
         }
+
+        // Update spot light direction
+        spotAngle += spotInc * 0.05f;
+        if (spotAngle > 2) {
+            spotInc = -1;
+        } else if (spotAngle < -2) {
+            spotInc = 1;
+        }
+        double spotAngleRad = Math.toRadians(spotAngle);
+        Vector3f coneDir = spotLightList[0].getConeDirection();
+        coneDir.y = (float) Math.sin(spotAngleRad);
+
+        // Update directional light direction, intensity and colour
+        lightAngle += 1.1f;
+        if (lightAngle > 90) {
+            directionalLight.setIntensity(0);
+            if (lightAngle >= 360) {
+                lightAngle = -90;
+            }
+        } else if (lightAngle <= -80 || lightAngle >= 80) {
+            float factor = 1 - (float)(Math.abs(lightAngle) - 80)/ 10.0f;
+            directionalLight.setIntensity(factor);
+            directionalLight.getColor().y = Math.max(factor, 0.9f);
+            directionalLight.getColor().z = Math.max(factor, 0.5f);
+        } else {
+            directionalLight.setIntensity(1);
+            directionalLight.getColor().x = 1;
+            directionalLight.getColor().y = 1;
+            directionalLight.getColor().z = 1;
+        }
+        double angRad = Math.toRadians(lightAngle);
+        directionalLight.getDirection().x = (float) Math.sin(angRad);
+        directionalLight.getDirection().y = (float) Math.cos(angRad);
     }
 
     @Override
     public void render(Window window) {
-        renderer.render(window, camera, gameItems, ambientLight, pointLight);
+        renderer.render(window, camera, gameItems, ambientLight, pointLightList, spotLightList, directionalLight);
     }
 
     @Override
